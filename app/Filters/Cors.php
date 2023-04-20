fbb9a3ba5ef5a20ba63371f5e8b7d3d7f7ce8921
Apvictor <armando2019ti@gmail.com>
2023-04-20 18:58:27 -0300

    fix

--- app/Filters/Cors.php
+++ app/Filters/Cors.php
@@ -10,11 +10,16 @@ class Cors implements FilterInterface
 {
   public function before(RequestInterface $request, $arguments = null)
   {
-    header('Access-Control-Allow-Origin: *');
-    header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+    // header('Access-Control-Allow-Origin: *');
+    // header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+    // header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+    // $method = $_SERVER['REQUEST_METHOD'];
+    // if ($method == "OPTIONS") {
+    //   die();
+    // }
     $method = $_SERVER['REQUEST_METHOD'];
     if ($method == "OPTIONS") {
+      header("HTTP/1.1 200 OK");
       die();
     }
   }
