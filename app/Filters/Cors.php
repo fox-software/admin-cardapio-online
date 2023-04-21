ce069b46e9186b7e8ecb470d7971e11124206a8a
Apvictor <armando2019ti@gmail.com>
2023-04-20 21:12:23 -0300

    fix

--- app/Filters/Cors.php
+++ app/Filters/Cors.php
@@ -28,11 +28,11 @@ class Cors implements FilterInterface
       header('Access-Control-Allow-Origin: ' . $origin);
     }
 
-    // header("Access-Control-Allow-Headers: Origin, X-API-KEY, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Access-Control-Allow-Headers, Authorization, observe, enctype, Content-Length, X-Csrf-Token");
-    // header("Access-Control-Allow-Methods: GET, PUT, POST, DELETE, PATCH, OPTIONS");
-    // header("Access-Control-Allow-Credentials: true");
-    // header("Access-Control-Max-Age: 3600");
-    // header('content-type: application/json; charset=utf-8');
+    header("Access-Control-Allow-Headers: Origin, X-API-KEY, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Access-Control-Allow-Headers, Authorization, observe, enctype, Content-Length, X-Csrf-Token");
+    header("Access-Control-Allow-Methods: GET, PUT, POST, DELETE, PATCH, OPTIONS");
+    header("Access-Control-Allow-Credentials: true");
+    header("Access-Control-Max-Age: 3600");
+    header('content-type: application/json; charset=utf-8');
     $method = $_SERVER['REQUEST_METHOD'];
     if ($method == "OPTIONS") {
       header("HTTP/1.1 200 OK CORS");
