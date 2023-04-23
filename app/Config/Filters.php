867cdae3aa3c2d420fb347ebfa9583d09795d3e1
Apvictor <armando2019ti@gmail.com>
2023-04-23 20:03:21 -0300

    fix

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -3,14 +3,15 @@
 namespace Config;
 
 use App\Filters\AuthGuard;
-// use App\Filters\Cors;
+use App\Filters\Cors;
+
 use CodeIgniter\Config\BaseConfig;
 use CodeIgniter\Filters\CSRF;
 use CodeIgniter\Filters\DebugToolbar;
 use CodeIgniter\Filters\Honeypot;
 use CodeIgniter\Filters\InvalidChars;
 use CodeIgniter\Filters\SecureHeaders;
-use Fluent\Cors\Filters\CorsFilter;
+// use Fluent\Cors\Filters\CorsFilter;
 
 class Filters extends BaseConfig
 {
@@ -24,8 +25,8 @@ class Filters extends BaseConfig
         'honeypot'      => Honeypot::class,
         'invalidchars'  => InvalidChars::class,
         'secureheaders' => SecureHeaders::class,
-        'cors'          => CorsFilter::class,
-        // 'cors'          => Cors::class,
+        // 'cors'          => CorsFilter::class,
+        'cors'          => Cors::class,
         'authGuard'     => AuthGuard::class,
     ];
 
