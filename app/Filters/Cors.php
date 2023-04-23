24f07615ed51a1a9fe8d4de34bc8414c4feeaa67
Apvictor <armando2019ti@gmail.com>
2023-04-23 19:55:55 -0300

    fix

--- app/Filters/Cors.php
+++ app/Filters/Cors.php
@@ -10,8 +10,7 @@ class Cors implements FilterInterface
 {
   public function before(RequestInterface $request, $arguments = null)
   {
-    header('Access-Control-Allow-Origin: https://app-cardapio-online.vercel.app');
-    header('Access-Control-Allow-Origin: http://localhost');
+    header('Access-Control-Allow-Origin: https://app-cardapio-online.vercel.app, http://localhost');
     
     header("Access-Control-Allow-Headers: Content-Type, Accept, Authorization, Sistema");
     header("Access-Control-Allow-Methods: GET, PUT, POST, DELETE, PATCH, OPTIONS");
